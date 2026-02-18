4a1fd90019e1abffc7109927e2ae85c5d979104e
João Tonini <jtonini@richmond.edu>
2026-02-18 13:44:49 -0500

    Add DEPLOY_DIR to PATH so local vmtouch is found first on Rocky 8

--- orchestrator.sh
+++ orchestrator.sh
@@ -89,6 +89,7 @@ do_start() {
                 DURATION_HR='${DURATION_HR}' \
                 PING_COUNT='${PING_COUNT}' \
                 SOURCE_LABEL='${host}' \
+                PATH="${DEPLOY_DIR}:${PATH}"
                 RESULTS_DIR='./collector_results' \
             bash ./collector.sh '${DEST}' \
                 > ${DEPLOY_DIR}/collector_${host}.log 2>&1 </dev/null &
